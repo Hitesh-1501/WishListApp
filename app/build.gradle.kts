4889809da5ff8e09ac8d412ef52a5c4290fb85d2
hitesh <badgujarhitesh1501@gmail.com>
2024-08-19 09:14:37 +0530

    add gradle dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview:$compose_version")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-
+    implementation ("androidx.activity:activity-ktx:1.7.1")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
     implementation("androidx.activity:activity-compose:1.9.0")
